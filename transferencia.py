a3ee34b8c1525c2edc0c187acbfdc35251b177d2
PauloAlbqrq <phximenes38@gmail.com>
2025-01-22 18:03:43 -0400

    Pequenos ajustes e implementação do threading

--- transferencia.py
+++ transferencia.py
@@ -2,7 +2,7 @@ import datetime
 from conta import conta
 
 class transferencia:
-    def _init_(self):
+    def __init__(self):
         self.log = []
 
         self.transaction_id = 0
@@ -15,7 +15,7 @@ class transferencia:
         except FileExistsError:
             arquivoLog = open("log.txt", "a")
         
-        arquivoLog.write(f"[{datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] R${valor} transferido de {contaOrigem.getID()} para {contaDestino.getID()} | Saldo final de {contaOrigem.getID()}: R${contaOrigem.getSaldo()} | Saldo final de {contaDestino.getID()}: R${contaDestino.getSaldo()}\n")
+        arquivoLog.write(f"[{datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')}] R${valor:.2f} transferido de {contaOrigem.getID()} para {contaDestino.getID()} | Saldo final de {contaOrigem.getID()}: R${contaOrigem.getSaldo():.2f} | Saldo final de {contaDestino.getID()}: R${contaDestino.getSaldo():.2f}\n")
         arquivoLog.close()
 
     def transferir(self, conta_origem, conta_destino, valor):
@@ -32,8 +32,8 @@ class transferencia:
                 if conta_origem.sacar(valor):
                     conta_destino.depositar(valor)
                     self.logWrite(conta_origem, conta_destino, valor)
-                    print(f"Transferência concluída.\nSaldo final de {conta_origem.getID()}: R${conta_origem.getSaldo()}\nSaldo final de {conta_destino.getID()}: R${conta_destino.getSaldo()}")
+                    print(f"Transferência concluída.\nSaldo final de {conta_origem.getID()}: R${conta_origem.getSaldo():.2f}\nSaldo final de {conta_destino.getID()}: R${conta_destino.getSaldo():.2f}")
                 else:
-                    print(f"Saldo insuficiente para transferência.\nSaldo atual de {conta_origem.getID()}: R${conta_origem.getSaldo()}")
+                    print(f"Saldo insuficiente para transferência.\nSaldo atual de {conta_origem.getID()}: R${conta_origem.getSaldo():.2f}")
 
 
